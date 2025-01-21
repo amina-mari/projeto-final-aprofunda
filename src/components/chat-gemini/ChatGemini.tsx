c5271289b512d34a4fc11866aa381fee1717a049
Mariana Amina <mariana.dantasweb7@gmail.com>
2025-01-21 17:03:19 -0300

    adding changes

--- src/components/chat-gemini/ChatGemini.tsx
+++ src/components/chat-gemini/ChatGemini.tsx
@@ -1,8 +1,12 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import * as S from './styles'
 import {AiOutlineMessage} from 'react-icons/ai'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../services/firebase';
+import http from '../../http';
 
 export default function ChatGemini({despesas}: {despesas: any}) {
+    const [user] = useAuthState(auth);
     const [isOpen, setIsOpen] = useState(false);
     const [messages, setMessages] = useState<{sender: string; text: string}[]>([]);
     const [input, setInput] = useState("")
@@ -11,23 +15,60 @@ export default function ChatGemini({despesas}: {despesas: any}) {
         setIsOpen(!isOpen);
     }
 
+    useEffect(() => {
+        if(user?.uid){
+            createSession()
+        }
+    }, [user])
+
+    const createSession = async () => {
+        try {
+            const response = await http.post("/chat", {
+                uid: user?.uid,
+                message: "Iniciando a conversa"
+            })
+
+            setMessages([
+                ...response.data.messages.map((msg: any) => ({
+                    sender: msg.sender,
+                    text: msg.content
+                }))
+            ])
+        } catch(error) {
+            console.error("Erro ao criar a sessão", error);
+        }
+    }
+
     const sendMessage = async () => {
         if(!input.trim()) return;
 
-        setMessages(prev => [...prev, {sender: "user", text: input}])
+        const userMessage = {sender: "user", text: input}
+
+        setMessages(prev => [...prev, userMessage])
+
+        setInput("");
 
         try {
-            const response = await fetch("/api/gemini-chat", {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({message: input, despesas})
-            });
-            const data = await response.json();
+            // const response = await fetch("/api/gemini-chat", {
+            //     method: "POST",
+            //     headers: {"Content-Type": "application/json"},
+            //     body: JSON.stringify({message: input, despesas})
+            // });
+
+            const response = await http.post("/chat", {
+                uid: user?.uid,
+                message: input
+            })
+
+            const botMessage = await response.data.messages?.[response.data.messages.length - 1].content;
+
+            if(botMessage) {
+                setMessages(prev => [...prev, {sender: "bot", text: botMessage}])
+            }
        
-            setMessages(prev => [...prev, {sender: "bot", text: data.reply}])
         }
         catch (err) {
-            console.error("Erro ao se comunicar com a API do Gemini", err)
+            console.error("Erro ao enviar a mensagem", err)
             setMessages(prev => [...prev, {sender: "bot", text: "Erro ao te responder"}])
         }
 
